f30fa7ad85d0af5442e0476c7018fde1b07eca54
tykiblood <tykiblood1992@hotmail.com>
2018-08-16 03:12:55 -0500

    server js

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-var path = require('path');
 var express = require('express');
-
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// route all urls to vue js router
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
-})
-
-app.set('port', process.env.PORT || 8080);
-app.set('NODE_ENV', 'production');
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('server started '+ port);
