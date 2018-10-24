be9db72e1c2e46864098babaf50562085edd8a55
Sebastian <ingesql1992@hotmail.com>
2018-10-24 02:45:22 -0500

    Testing

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  TEST: '"' + process.env.TEST + '"'
+  NODE_ENV: '"development"'
 })
